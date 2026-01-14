8f8941410cef275a58aa4ea8b12ca506df0b20b9
Isaiah Rohan Perez <rohanperez01@gmail.com>
2026-01-15 06:14:07 +0800

    Add files via upload

---
+++ RunProject.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Runs the R script using Rscript.exe
+"C:\Program Files\R\R-4.5.1\bin\x64\Rscript.exe" "C:\Users\Rohan Perez\Documents\InSite Final Project.R"
+
+:: Pauses so you can see if there are any errors before closing
+PAUSE
